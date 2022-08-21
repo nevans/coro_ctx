a09754cea0f323394f2e8e785e5fd11032cfb96b
nicholas a. evans <nicholas.evans@gmail.com>
2022-08-21 17:26:24 -0400

    ✅ first examples passing
    I've written this from scratch multiple times now.  I'm starting fresh
    to try to focus first on the core/primary use-cases.
    
    And, although this is missing fiber/thread/ractor inheritance, typed
    elements, performance optimizations, and coroutine scoping, the basic
    core behavior is simple and it works with just this.
    
    Additionally, I'm implementing all Kernel changes as refinements, which
    feels very appropriate to me.

--- lib/coro_ctx.rb
+++ lib/coro_ctx.rb
@@ -2,7 +2,34 @@
 
 require_relative "coro_ctx/version"
 
+# Context values with dynamic scope, inherited by fibers, threads, and even
+# ractors.
+#
+# TODO: documentation...
 module CoroCtx
   class Error < StandardError; end
-  # Your code goes here...
+
+  CURRENT_CTX_VAR_NAME = :__CORO_CTX_CURRENT_CTX_VAR_NAME__
+  private_constant :CURRENT_CTX_VAR_NAME
+
+  refine Kernel do
+    def with_ctx_values(...) = CoroCtx.with_values(...)
+  end
+
+  def self.[](key)
+    ctx = Thread.current[CURRENT_CTX_VAR_NAME]
+    ctx&.[](key)
+  end
+
+  def self.with_values(**named_elements)
+    thr = Thread.current
+    original_ctx = thr[CURRENT_CTX_VAR_NAME]
+    ctx = {**(original_ctx || {}), **named_elements}.freeze
+    thr[CURRENT_CTX_VAR_NAME] = ctx
+    raise ArgumentError, "expected block" unless block_given?
+    yield
+  ensure
+    thr[CURRENT_CTX_VAR_NAME] = original_ctx
+  end
+
 end
--- spec/coro_ctx_spec.rb
+++ spec/coro_ctx_spec.rb
@@ -1,11 +1,63 @@
 # frozen_string_literal: true
 
+# This spec provides overview examples, so some rubocop rules don't apply.
+# rubocop:disable RSpec/DescribedClass, RSpec/MultipleExpectations
+# rubocop:disable RSpec/ExampleLength
+
 RSpec.describe CoroCtx do
   it "has a version number" do
     expect(CoroCtx::VERSION).not_to be_nil
   end
 
-  it "provides dynamically scoped global variables"
+  using CoroCtx
+
+  it "provides dynamically scoped global variables" do
+    expect([CoroCtx[:foo], CoroCtx[:hoge]]).to eq([nil, nil])
+    retval = with_ctx_values(foo: :foo) {
+      expect([CoroCtx[:foo], CoroCtx[:hoge]]).to eq([:foo, nil])
+      foo_bar_baz_quux_hoge_fuga do
+        expect([CoroCtx[:foo], CoroCtx[:hoge]]).to eq([:quux, :fuga])
+      end
+      expect([CoroCtx[:foo], CoroCtx[:hoge]]).to eq([:foo, nil])
+      :executed_block
+    }
+    expect(retval).to equal(:executed_block)
+    expect([CoroCtx[:foo], CoroCtx[:hoge]]).to eq([nil, nil])
+  end
+
+  # these methods demonstrate that context values follow stack-frames
+  # rubocop:disable Metrics/AbcSize
+  def foo_bar_baz_quux_hoge_fuga(&)
+    expect([CoroCtx[:foo], CoroCtx[:hoge]]).to eq([:foo, nil])
+    with_ctx_values foo: :bar, hoge: :hoge do
+      expect([CoroCtx[:foo], CoroCtx[:hoge]]).to eq([:bar, :hoge])
+      bar_baz_quux_fuga(&)
+      expect([CoroCtx[:foo], CoroCtx[:hoge]]).to eq([:bar, :hoge])
+    end
+    expect([CoroCtx[:foo], CoroCtx[:hoge]]).to eq([:foo, nil])
+  end
+
+  def bar_baz_quux_fuga(&)
+    expect([CoroCtx[:foo], CoroCtx[:hoge]]).to eq([:bar, :hoge])
+    with_ctx_values foo: :baz, hoge: nil do
+      expect([CoroCtx[:foo], CoroCtx[:hoge]]).to eq([:baz, nil])
+      baz_quux_fuga(&)
+      expect([CoroCtx[:foo], CoroCtx[:hoge]]).to eq([:baz, nil])
+    end
+    expect([CoroCtx[:foo], CoroCtx[:hoge]]).to eq([:bar, :hoge])
+  end
+
+  def baz_quux_fuga
+    expect([CoroCtx[:foo], CoroCtx[:hoge]]).to eq([:baz, nil])
+    with_ctx_values foo: :quux, hoge: :fuga do
+      expect([CoroCtx[:foo], CoroCtx[:hoge]]).to eq([:quux, :fuga])
+      yield
+      expect([CoroCtx[:foo], CoroCtx[:hoge]]).to eq([:quux, :fuga])
+    end
+    expect([CoroCtx[:foo], CoroCtx[:hoge]]).to eq([:baz, nil])
+  end
+
+  # rubocop:enable Metrics/AbcSize
 
   it "is inherited by enumerator fibers"
   it "is inherited by new fibers"
@@ -20,3 +72,6 @@ RSpec.describe CoroCtx do
     it "is inherited by new ractors"
   end
 end
+
+# rubocop:enable RSpec/DescribedClass, RSpec/MultipleExpectations
+# rubocop:enable RSpec/ExampleLength
