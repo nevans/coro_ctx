5551a399eaddd9d2529c16b23a09ff730458aba4
nicholas a. evans <nicholas.evans@gmail.com>
2022-09-06 13:12:20 -0400

    wip: cached

--- lib/coro_ctx.rb
+++ lib/coro_ctx.rb
@@ -1,46 +1,27 @@
 # frozen_string_literal: true
 
-require_relative "coro_ctx/version"
-
 # Context values with dynamic scope, inherited by fibers, threads, and even
 # ractors.
 #
 # TODO: documentation...
 module CoroCtx
-  autoload :Inheritance,     "#{__dir__}/coro_ctx/inheritance"
+  autoload :Current,         "#{__dir__}/coro_ctx/current"
   autoload :RactorDelegator, "#{__dir__}/coro_ctx/ractor_delegator"
   autoload :RactorMediator,  "#{__dir__}/coro_ctx/ractor_mediator"
+  autoload :VERSION,         "#{__dir__}/coro_ctx/version"
 
   class Error < StandardError; end
-
-  CURRENT_CTX_VAR_NAME = :__CORO_CTX_CURRENT_CTX_VAR_NAME__
-  private_constant :CURRENT_CTX_VAR_NAME
 
   EMPTY = {}.freeze
   private_constant :EMPTY
 
   refine Kernel do
-    def with_ctx_values(...) = CoroCtx.with_values(...)
+    def with_ctx_values(...) = Current.with_values(...)
   end
 
-  def self.[](key)
-    ctx = Thread.current[CURRENT_CTX_VAR_NAME]
-    ctx&.[](key)
-  end
-
-  def self.with_values(**named_elements)
-    thr = Thread.current
-    original_ctx = thr[CURRENT_CTX_VAR_NAME]
-    ctx = {**(original_ctx || EMPTY), **named_elements}.freeze
-    thr.thread_variable_set(CURRENT_CTX_VAR_NAME, ctx)
-    thr[CURRENT_CTX_VAR_NAME] = ctx
-    raise ArgumentError, "expected block" unless block_given?
-    yield
-  ensure
-    thr[CURRENT_CTX_VAR_NAME] = original_ctx
-    thr.thread_variable_set(CURRENT_CTX_VAR_NAME, original_ctx)
-  end
+  def self.[](...)          = Current.[](...)
+  def self.with_values(...) = Current.with_values(...)
 
 end
 
-CoroCtx::Inheritance.extend_core!
+CoroCtx::Current.extend_core!
---
+++ lib/coro_ctx/current.rb
@@ -0,0 +1,110 @@
+# frozen_string_literal: true
+
+module CoroCtx
+
+  # Defines the "current" CoroCtx, with accessor methods to query and update it.
+  #
+  module Current
+    CTX_VAR_NAME = :__CORO_CTX_CURRENT_CTX_VAR_NAME__
+    private_constant :CTX_VAR_NAME
+
+    # :nodoc:
+    module Impl
+      refine Fiber.singleton_class do
+        # TODO: fallback for non-main ractors
+        def current_coro_ctx
+          thr = Thread.current
+          thr[CTX_VAR_NAME] ||=
+            thr.thread_variable_get(CTX_VAR_NAME) ||
+            EMPTY
+        end
+
+        def __current_coro_ctx__=(ctx)
+          thr = Thread.current
+          thr[CTX_VAR_NAME] = ctx
+          thr.thread_variable_set(CTX_VAR_NAME, ctx)
+        end
+      end
+
+      # TODO: linked-list context object; not just a hash
+      refine Hash do
+        def with_values(**named_elements, &)
+          ctx = merge(named_elements).freeze
+          return ctx unless block_given?
+          ctx.__exec__(&)
+        end
+
+        def __exec__
+          prior = Fiber.current_coro_ctx
+          Fiber.__current_coro_ctx__ = self
+          yield
+        ensure
+          Fiber.__current_coro_ctx__ = prior
+        end
+      end
+    end
+    private_constant :Impl
+    using Impl
+
+    def self.[](...)   = Fiber.current_coro_ctx.[](...)
+    def self.to_h(...) = Fiber.current_coro_ctx.to_h(...)
+
+    module_function
+
+    def with_values(...) = Fiber.current_coro_ctx.with_values(...)
+
+    # TODO: how to re-establish trace in new ractors?
+    def self.extend_core!
+      ::Fiber.singleton_class.instance_exec  { prepend FiberNewInheritance }
+      ::Thread.singleton_class.instance_exec { prepend ThreadNewInheritance }
+      ::Ractor.singleton_class.instance_exec { prepend RactorNewInheritance }
+      fiber_switch_trace
+    end
+
+    # To handle fibers created by the C API bypassing Fiber.new—e.g.
+    # implicit Enumerator fibers—we use a thread variable as backup and use
+    # +fiber_switch+ to synchronize with the fiber local variable.
+    def self.fiber_switch_trace
+      Ractor.current[:coro_ctx_trace] ||= TracePoint.new(:fiber_switch) do |tp|
+        thr = Thread.current
+        if (fvar = thr[CTX_VAR_NAME])
+          # This handles e.g. fibers that were suspended and resumed.
+          thr.thread_variable_set(CTX_VAR_NAME, fvar)
+        elsif (tvar = thr.thread_variable_get(CTX_VAR_NAME))
+          # This handles fibers that were created bypassing Fiber.new, e.g.
+          # implicit Enumerator fibers.
+          thr[CTX_VAR_NAME] = tvar
+        end
+      end
+    end
+    private_class_method :fiber_switch_trace
+
+    # When created via Fiber.new, we want the context from *definition*, not
+    # from the first resume (as a +fiber_switch+ implementation would do).
+    module FiberNewInheritance
+      def new(ctx: {}, **fiber_opts, &block)
+        prior_ctx = Thread.current[CTX_VAR_NAME] || EMPTY
+        super(**fiber_opts) { CoroCtx.with_values(**prior_ctx, **ctx, &block) }
+      end
+    end
+
+    # When created via Thread.new, we'll get the context from *definition*.
+    module ThreadNewInheritance
+      def new(*args, **kwargs, &block)
+        ctx = Thread.current[CTX_VAR_NAME] || EMPTY
+        super() { CoroCtx.with_values(**ctx) { block.call(*args, **kwargs) } }
+      end
+    end
+
+    # registers the initial context for all new ractors
+    module RactorNewInheritance
+      def new(...)
+        ctx = Thread.current[CTX_VAR_NAME]
+        raise Ractor::Error, "Unshareable CoroCtx" unless Ractor.shareable?(ctx)
+        return super unless RactorMediator.started?
+        super.tap do |r| RactorMediator.new_ractor r, ctx end
+      end
+    end
+
+  end
+end
